ddbcaab11c6d7791be783571a3b1d9c9a4098c2c
carlos3g <carlosmesquita156@gmail.com>
2021-03-23 15:07:00 -0300

    [FEAT] uses custom fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Montserrat_400Regular } from '@expo-google-fonts/montserrat';
 
 import { Routes } from './navigation';
 
 function App() {
+  let [fontsLoaded] = useFonts({ Montserrat_400Regular });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar translucent />
