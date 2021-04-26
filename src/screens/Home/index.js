8a453fb745c100fd4b9a7f1b62b004df640bb3bf
carlos3g <carlosmesquita156@gmail.com>
2021-04-26 16:47:25 -0300

    [REFACTOR] uses Toast component to show alerts

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 /**
- * TODO: Change alert on status not granted
  * TODO: Lets code more clean
  */
 
@@ -10,59 +9,62 @@ import { useIsDrawerOpen } from '@react-navigation/drawer';
 import * as ImagePicker from 'expo-image-picker';
 import axios from '@services/api';
 
-import { GridMenu, SearchButton, AnimeCard } from '@components/';
+import { GridMenu, SearchButton, AnimeCard, Toast } from '@components/';
 import { Container, Wrapper, Modal, ActivityIndicator } from './styles';
-
-async function requestCameraPermission() {
-  const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-  if (status !== 'granted') {
-    alert('Desculpa, nós precisamos de acesso as imagens para funcionar!');
-  }
-}
-
-async function pickImage(setter) {
-  const result = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    allowsEditing: true,
-    quality: 1,
-  });
-
-  if (!result.cancelled) {
-    const name = result.uri.split('/').pop(); // "file:/[...]/images/image.png" => "image.png"
-    setter({ ...result, name });
-  }
-}
-
-function searchAnime(image) {
-  return new Promise(async (resolve, reject) => {
-    const formData = new FormData();
-
-    formData.append('image', {
-      uri: image.uri,
-      name: image.name,
-      type: 'image/jpg',
-    });
-
-    try {
-      const { data } = await axios.post('/search', formData);
-      const imageResult = {
-        name: data.docs[0].title_romaji,
-        imgUrl: image.uri,
-        id: data.docs[0].tokenthumb,
-      };
-      return resolve(imageResult);
-    } catch (error) {
-      console.log(error);
-      return reject(error);
-    }
-  });
-}
 
 function HomeScreen() {
   const { toggleDrawer } = useNavigation();
   const [image, setImage] = useState(null);
   const [anime, setAnime] = useState(null);
   const modalRef = useRef(null);
+  const toastRef = useRef(null);
+
+  async function requestCameraPermission() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      toastRef.current.show('Sorry, we need media permission to work!');
+    }
+  }
+
+  async function pickImage() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      const name = result.uri.split('/').pop(); // "file:/[...]/images/image.png" => "image.png"
+      setImage({ ...result, name });
+    }
+  }
+
+  function searchAnime() {
+    return new Promise(async (resolve, reject) => {
+      const formData = new FormData();
+
+      formData.append('image', {
+        uri: image.uri,
+        name: image.name,
+        type: 'image/jpg',
+      });
+
+      try {
+        const { data } = await axios.post('/search', formData);
+        const imageResult = {
+          name: data.docs[0].title_romaji,
+          imgUrl: image.uri,
+          id: data.docs[0].tokenthumb,
+        };
+        return resolve(imageResult);
+      } catch (error) {
+        toastRef.current.show(
+          'We had a problem looking for this image. Try again or use another image!'
+        );
+        return reject(error);
+      }
+    });
+  }
 
   useEffect(() => {
     requestCameraPermission();
@@ -89,6 +91,7 @@ function HomeScreen() {
         <Modal ref={modalRef}>
           <AnimeCard anime={anime} />
         </Modal>
+        <Toast ref={toastRef} timeout={3200} />
       </Container>
     </Wrapper>
   );
